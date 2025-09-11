7bbc59ed010f75b83ff24902ae9e35f326d6b5d7
shaheerkhan <shaheerakhan94@gmail.com>
2025-09-10 20:09:48 -0700

    add setup.py

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="astronautics-usc",
+    version="0.1.0",
+    description="Collection of space and orbital mechanics functions for ASTE 520 coursework",
+    author="Shaheer Khan",
+    packages=find_packages(where=".", exclude=["test"]),
+    install_requires=[
+        "numpy>=1.20",
+        "scipy>=1.7",
+        "matplotlib>=3.4",
+        # add others if you use them
+    ],
+    python_requires=">=3.8",
+)
