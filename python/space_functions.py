2ec61132d772d95741ad701f2ad2a3772ca090d8
shaheerkhan <shaheerakhan94@gmail.com>
2025-09-10 20:09:58 -0700

    problem 4-3

--- python/space_functions.py
+++ python/space_functions.py
@@ -17,3 +17,46 @@ def apparent_magnitude(r: Distance, abs_magnitude: float) -> float:
     """
     r_pc = r.to(ureg.parsec).magnitude  # convert to parsecs
     return abs_magnitude - 5 + 5 * np.log10(r_pc)
+
+
+def planet_distance(lam1_deg, beta1_deg, r1, lam2_deg, beta2_deg, r2):
+    """
+    Compute the heliocentric distance between two planets given in
+    ecliptic coordinates using the spherical law of cosines.
+
+    Parameters
+    ----------
+    lam1_deg, lam2_deg : float
+        Ecliptic longitudes (degrees)
+    beta1_deg, beta2_deg : float
+        Ecliptic latitudes (degrees)
+    r1, r2 : float
+        Heliocentric distances (AU)
+
+    Returns
+    -------
+    S_AU : float
+        Distance between the two planets (AU)
+    S_km : float
+        Distance between the two planets (km)
+    """
+
+    # Convert to radians
+    lam1 = np.deg2rad(lam1_deg)
+    beta1 = np.deg2rad(beta1_deg)
+    lam2 = np.deg2rad(lam2_deg)
+    beta2 = np.deg2rad(beta2_deg)
+
+    # Angular separation
+    cos_psi = (np.sin(beta1) * np.sin(beta2) +
+               np.cos(beta1) * np.cos(beta2) * np.cos(lam1 - lam2))
+
+    # Straight-line distance
+    S_AU = np.sqrt(r1**2 + r2**2 - 2 * r1 * r2 * cos_psi)
+
+    # Convert AU → km
+    AU_to_km = 149597870
+    S_km = S_AU * AU_to_km
+
+    return S_AU, S_km
+
